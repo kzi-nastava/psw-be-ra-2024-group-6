884d2613acd33b828d8b07309df7f40a9745f1b0
matejaaj <mateja.odzaci@gmail.com>
2024-10-11 16:59:52 +0200

    feat: Club Domain and Service Classes created

---
+++ src/Modules/Stakeholders/Explorer.Stakeholders.API/Dtos/ClubDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Stakeholders.API.Dtos
+{
+    public class ClubDto
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public long OwnerId { get; set; }
+        public List<long> MemberIds { get; set; }
+
+    }
+}
