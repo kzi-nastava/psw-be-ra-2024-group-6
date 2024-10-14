c3eff974ffec139e594b721f3515bf47ceb3b8c0
Aleksandar Tomic <tomic.ra198.2021@uns.ac.rs>
2024-10-14 11:50:21 +0200

    feat: Added Controller and infrastructure with migration for Tour entity

---
+++ src/Explorer.API/Controllers/Author/TourController.cs
@@ -0,0 +1,35 @@
+﻿using Explorer.BuildingBlocks.Core.UseCases;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Explorer.Tours.API.Dtos;
+using Explorer.Tours.API.Public;
+
+namespace Explorer.API.Controllers.Author
+{
+    [Authorize(Policy = "authorPolicy")]
+    [Route("api/tours")]
+    public class TourController : BaseApiController
+    {
+        private readonly ITourService _tourService;
+
+        public TourController(ITourService tourService)
+        {
+            _tourService = tourService;
+        }
+
+        [HttpGet]
+        public ActionResult<PagedResult<TourDto>> GetAll([FromQuery] int page, [FromQuery] int pageSize)
+        {
+            var result = _tourService.GetPaged(page, pageSize);
+            return CreateResponse(result);
+        }
+
+        [HttpPost]
+        public ActionResult<TourDto> Create([FromBody] TourDto tour)
+        {
+            var result = _tourService.Create(tour);
+            return CreateResponse(result);
+        }
+
+    }
+}
