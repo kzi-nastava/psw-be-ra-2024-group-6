1989a1d539703688d427c633f6e3668e70a362df
Luka Ivanisevic <lukaivanisevic10@gmail.com>
2024-10-14 19:10:16 +0200

    Added Checkpoint domain model, checkpoint service and his interface and created checkpoint dto

---
+++ src/Modules/Tours/Explorer.Tours.Core/Domain/Checkpoint.cs
@@ -0,0 +1,18 @@
+﻿using Explorer.BuildingBlocks.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Tours.Core.Domain;
+
+    public class Checkpoint : Entity
+    {
+        public int TourId { get; init; }
+        public int LocationId { get; init; }
+        public string Name { get; init; }
+        public string Description { get; init; }
+        public string ImageUrl { get; init; }
+    }
+
---
+++ src/Modules/Tours/Explorer.Tours.Core/UseCases/Administration/CheckpointService.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Explorer.BuildingBlocks.Core.UseCases;
+using Explorer.Tours.API.Dtos;
+using Explorer.Tours.Core.Domain;
+using Explorer.Tours.API.Public.Administration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Tours.Core.UseCases.Administration
+{
+    public class CheckpointService : CrudService<CheckpointDto, Checkpoint>,ICheckpointService
+    {
+        public CheckpointService(ICrudRepository<Checkpoint> crudRepository, IMapper mapper) : base(crudRepository, mapper){}
+    }
+}
