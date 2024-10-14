cd38830c65ec6ac3a9b63cdc2f89384ba45434f3
Aleksandar Tomic <tomic.ra198.2021@uns.ac.rs>
2024-10-14 09:25:09 +0200

    feat: created Tour Domain, Tour service, interfaces for service DTO and mapper

---
+++ src/Modules/Tours/Explorer.Tours.API/Dtos/TourDto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Tours.API.Dtos
+{
+    public enum Difficulty
+    {
+        Easy,
+        Medium,
+        Hard
+    }
+
+    public enum Status
+    {
+        Draft,
+        Published,
+        Closed
+    }
+    public class TourDto
+    {
+        public int TourId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Difficulty Difficulty { get; set; }
+        public List<string> Tags { get; set; } // Lista tagova koji opisuju turu
+        public double Cost { get; set; } = 0; // Default cena je 0
+        public Status Status { get; set; } = Status.Draft; // Default status je Draft
+    }
+}
---
+++ src/Modules/Tours/Explorer.Tours.Core/Domain/Tour.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Explorer.BuildingBlocks.Core.Domain;
+
+namespace Explorer.Tours.Core.Domain
+{
+    public enum Difficulty
+    {
+        Easy,
+        Medium,
+        Hard
+    }
+
+    public enum Status
+    {
+        Draft,
+        Published,
+        Closed
+    }
+
+    public class Tour : Entity
+    {
+        public int TourId { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set;}
+        public Difficulty Difficulty { get; private set; }
+        public List<string> Tags { get; private set; } // Lista tagova koji opisuju turu
+        public double Cost { get; private set; } = 0; // Default cena je 0
+        public Status Status { get; private set; } = Status.Draft; // Default status je Draft
+
+
+        public Tour(int tourId,string name, string description, Difficulty difficulty, List<string> tags)
+        {
+            TourId = tourId;
+            Name = name;
+            Description = description;
+            Difficulty = difficulty;
+            Tags = new List<string>(tags); // Kreira kopiju liste tagova
+
+            Validate();
+        }
+
+        private void Validate()
+        {
+            if (string.IsNullOrWhiteSpace(Name)) throw new ArgumentException("Name is required.");
+            if (string.IsNullOrWhiteSpace(Description)) throw new ArgumentException("Description is required.");
+            if (Tags == null || Tags.Count == 0) throw new ArgumentException("At least one tag is required.");
+        }
+
+    }
+}
