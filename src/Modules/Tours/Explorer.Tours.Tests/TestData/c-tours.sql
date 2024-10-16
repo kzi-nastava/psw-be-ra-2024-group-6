ea5c83c2c49cfb826206be2145bcc994fd5b08b1
Aleksandar Tomic <tomic.ra198.2021@uns.ac.rs>
2024-10-16 14:01:55 +0200

    test: Tests for CRUD operations of Tour model

--- src/Modules/Tours/Explorer.Tours.Tests/TestData/c-tours.sql
+++ src/Modules/Tours/Explorer.Tours.Tests/TestData/c-tours.sql
@@ -1,3 +1,14 @@
 INSERT INTO tours."Tours"(
-	"Id", "TourId", "Name", "Description", "Difficulty", "Tags", "Cost", "Status")
-	VALUES (9, 9, 'Tura1', 'Opis', 1 , 'adawd', 1.0, 1);
+	"Id", "Name", "Description", "Difficulty", "Tags", "Cost", "Status", "AuthorId")
+	VALUES (9, 'Tura', 'Opis', 0 ,'{{priroda,tvrdjava}}', 1.0, 1,1);
+INSERT INTO tours."Tours"(
+	"Id", "Name", "Description", "Difficulty", "Tags", "Cost", "Status", "AuthorId")
+	VALUES (3, 'Tura2', 'Opis2', 0 ,'{{muzej,park}}', 1.0, 1,1);
+INSERT INTO tours."Tours"(
+	"Id", "Name", "Description", "Difficulty", "Tags", "Cost", "Status", "AuthorId")
+	VALUES (5, 'Tura3', 'Opis3', 0 ,'{{park,priroda}}', 1.0, 1,1);
+INSERT INTO tours."Tours"(
+	"Id", "Name", "Description", "Difficulty", "Tags", "Cost", "Status", "AuthorId")
+	VALUES (6, 'Tura4', 'Opis4', 0 ,'{{park,tvrdjava}}', 1.0, 1,1);
+
+
