9d7cf0a708119ba98902d6ccb0787b52831ce885
avan1garde <ericnikola17@gmail.com>
2024-10-16 20:11:10 +0200

    feat: created Rating controller

---
+++ src/Explorer.API/Controllers/Tourist/RatingController.cs
@@ -0,0 +1,34 @@
+﻿using Explorer.BuildingBlocks.Core.UseCases;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Explorer.Stakeholders.API.Dtos;
+using Explorer.Stakeholders.API.Public;
+
+namespace Explorer.API.Controllers.Tourist
+{
+    [Authorize(Policy = "touristPolicy")]
+    [Route("api/ratings")]
+    public class RatingController : BaseApiController
+    {
+        private readonly IRatingService _ratingService;
+
+        public RatingController(IRatingService ratingService)
+        {
+            _ratingService = ratingService;
+        }
+
+        [HttpGet]
+        public ActionResult<PagedResult<RatingDto>> GetAll([FromQuery] int page, [FromQuery] int pageSize)
+        {
+            var result = _ratingService.GetPaged(page, pageSize);
+            return CreateResponse(result);
+        }
+
+        [HttpPost]
+        public ActionResult<RatingDto> Create([FromBody] RatingDto ratingDto)
+        {
+            var createdRating = _ratingService.Create(ratingDto);
+            return CreateResponse(createdRating);
+        }
+    }
+}
