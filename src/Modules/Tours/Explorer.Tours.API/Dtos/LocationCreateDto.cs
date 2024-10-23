705d197a264bc5eda522c9c593671a55bb13dff3
Aleksandar Tomic <tomic.ra198.2021@uns.ac.rs>
2024-10-23 16:00:24 +0200

    Added LocationCreateDto

---
+++ src/Modules/Tours/Explorer.Tours.API/Dtos/LocationCreateDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Tours.API.Dtos
+{
+    public class LocationCreateDto
+    {
+        public long Latitude { get; set; }
+        public long Longitude { get; set; }
+
+        public LocationCreateDto(long latitude, long longitude)
+        {
+            
+            Latitude = latitude;
+            Longitude = longitude;
+        }
+    }
+}
