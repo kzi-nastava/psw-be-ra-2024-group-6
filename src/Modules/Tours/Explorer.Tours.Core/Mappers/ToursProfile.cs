7e6656a6f110d015aa6678ebd3056b7616113936
Aleksandar Tomic <tomic.ra198.2021@uns.ac.rs>
2024-10-23 15:55:12 +0200

    Modified ToursProfile in Mappers

--- src/Modules/Tours/Explorer.Tours.Core/Mappers/ToursProfile.cs
+++ src/Modules/Tours/Explorer.Tours.Core/Mappers/ToursProfile.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using Explorer.Tours.API.Dtos;
 using Explorer.Tours.Core.Domain;
+using Object = Explorer.Tours.Core.Domain.Object;
 
 namespace Explorer.Tours.Core.Mappers;
 
@@ -14,5 +15,20 @@ public class ToursProfile : Profile
 
         CreateMap<RequiredEquipmentDto, RequiredEquipment>().ReverseMap();
         CreateMap<TouristEquipmentManagerDto, TouristEquipmentManager>().ReverseMap();
+
+        //CreateMap<TourInfoDto, Tour>().ForMember(dest=>dest.Name,opt=>opt.MapFrom(src=>src.Name));
+        CreateMap<TourCreateDto, Tour>()
+            .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.TourInfo.Name))
+            .ForMember(dest => dest.AuthorId, opt => opt.MapFrom(src => src.TourInfo.AuthorId))
+            .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.TourInfo.Status))
+            .ForMember(dest => dest.Difficulty, opt => opt.MapFrom(src => src.TourInfo.Difficulty))
+            .ForMember(dest => dest.Description, opt => opt.MapFrom(src => src.TourInfo.Description))
+            .ForMember(dest => dest.Tags, opt => opt.MapFrom(src => src.TourInfo.Tags));
+
+
+
+        CreateMap<CheckpointCreateDto,Checkpoint>().ReverseMap();
+        CreateMap<ObjectCreateDto,Object>().ReverseMap();
+        
     }
 }
