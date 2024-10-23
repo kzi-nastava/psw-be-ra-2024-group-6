5d3437d7d4c6349801505a73dbaa35a324be7e0a
MilanUD <149141569+MilanUD@users.noreply.github.com>
2024-10-24 01:19:06 +0200

    fix: fixed bugs

--- src/Modules/Blog/Explorer.Blog.API/Dtos/BlogDto.cs
+++ src/Modules/Blog/Explorer.Blog.API/Dtos/BlogDto.cs
@@ -12,15 +12,9 @@ namespace Explorer.Blog.API.Dtos
         public string? Title { get; set; }
         public string? Description { get; set; }
         public DateTime CreatedAt { get; set; }
-        public Status? Status { get; set; }
+        public string Status { get; set; }
         public int? UserId { get; set; }
         public List<BlogPictureDto> Pictures { get; set; }
     }
 
-    public enum Status
-    {
-        Draft,
-        Closed,
-        Published
-    }
 }
