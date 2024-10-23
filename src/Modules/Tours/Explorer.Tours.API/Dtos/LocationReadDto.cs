c43d5c1a8b11d4f2462b3ed2fff250649e848189
Luka Ivanisevic <lukaivanisevic10@gmail.com>
2024-10-23 12:55:43 +0200

    Added dtos for reading location and checkpoint

---
+++ src/Modules/Tours/Explorer.Tours.API/Dtos/LocationReadDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Tours.API.Dtos
+{
+    public class LocationReadDto
+    {
+        public long Id { get; set; }
+        public long Latitude { get; set; }
+        public long Longitude { get; set; }
+    }
+}
