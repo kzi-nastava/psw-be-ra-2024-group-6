aaab48be8b7b79b75ab42ed9b9977c5200a41bcf
Aleksandar Tomic <tomic.ra198.2021@uns.ac.rs>
2024-10-23 16:00:47 +0200

    Added TourInfoDto

---
+++ src/Modules/Tours/Explorer.Tours.API/Dtos/TourInfoDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Tours.API.Dtos
+{
+    
+
+    
+    public class TourInfoDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Difficulty Difficulty { get; set; }
+        public List<string> Tags { get; set; } = new List<string>(); // Lista tagova koji opisuju turu
+        public double Cost { get; set; } = 0; // Default cena je 0
+        public Status Status { get; set; } = Status.Draft; // Default status je Draft
+        public long AuthorId { get; set; }
+    }
+}
