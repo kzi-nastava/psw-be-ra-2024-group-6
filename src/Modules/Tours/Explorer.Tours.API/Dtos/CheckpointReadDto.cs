a6654bc91049a8abca7443bb0d13b9e6a6728d3d
Aleksandar Tomic <tomic.ra198.2021@uns.ac.rs>
2024-10-23 16:02:44 +0200

    Added CheckpointReadDto , LocationReadDto and ObjectReadDto

---
+++ src/Modules/Tours/Explorer.Tours.API/Dtos/CheckpointReadDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Tours.API.Dtos
+{
+    public class CheckpointReadDto
+    {
+        
+        public int Id { get; set; }
+        public LocationReadDto Location { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+
+        public CheckpointReadDto(int id,LocationReadDto location,string name,string description,string imageUrl) {
+            Id = id;   
+            Location = location;
+            Name = name;
+            Description = description;
+            ImageUrl = imageUrl;
+        }
+    }
+}
---
+++ src/Modules/Tours/Explorer.Tours.API/Dtos/LocationReadDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Tours.API.Dtos
+{
+    public class LocationReadDto
+    {
+        public long Id { get; set; }
+
+        public long Latitude { get; set; }
+        public long Longitude { get; set; }
+
+        public LocationReadDto(long id,long latitude,long longitude) {
+            Id = id;
+            Latitude = latitude;
+            Longitude = longitude;
+        }
+    }
+}
