e6a3bb1ad4f756fb046363c15baa12d693954152
Milana Dobrić <milanadobric5@gmail.com>
2024-10-24 02:40:51 +0200

    fix

--- src/Explorer.API/Startup/AuthConfiguration.cs
+++ src/Explorer.API/Startup/AuthConfiguration.cs
@@ -56,8 +56,6 @@ public static class AuthConfiguration
             options.AddPolicy("authorPolicy", policy => policy.RequireRole("author"));
             options.AddPolicy("touristPolicy", policy => policy.RequireRole("tourist"));
             options.AddPolicy("touristOrAuthorPolicy", policy => policy.RequireRole("tourist", "author"));
-            options.AddPolicy("touristPolicy", policy => policy.RequireRole("administrator"));
-            options.AddPolicy("touristOrAuthorPolicy", policy => policy.RequireRole("tourist", "author"));
         });
     }
 }
