d2e2c1702221cdcfe8aadc615db78d4291d09337
Luka Ivanisevic <lukaivanisevic10@gmail.com>
2024-10-26 19:52:18 +0200

    Test data changed

--- src/Modules/Tours/Explorer.Tours.API/Dtos/CheckpointCreateDto.cs
+++ src/Modules/Tours/Explorer.Tours.API/Dtos/CheckpointCreateDto.cs
@@ -12,6 +12,6 @@ namespace Explorer.Tours.API.Dtos
         public string Name { get; set; }
         public string Description { get; set; }
         public string ImageUrl { get; set; }
-        public long TourId { get; set; }
+        public long? TourId { get; set; }
     }
 }
