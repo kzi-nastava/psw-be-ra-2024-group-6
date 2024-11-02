221ed6960c5951fcd2beeebc4e32c211457b441a
Luka Ivanisevic <lukaivanisevic10@gmail.com>
2024-11-02 01:21:09 +0100

    removed changes to testData

--- src/Modules/Stakeholders/Explorer.Stakeholders.Tests/TestData/g.sql
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Tests/TestData/g.sql
@@ -1,12 +1,9 @@
 ﻿INSERT INTO stakeholders."Problems"(
-    "Id", "Category", "Priority", "Date", "Description", "TourId", "TouristId")
-    VALUES (-1, 'kate', 'prvi', TO_TIMESTAMP('16-10-2024 10:10:10', 'DD-MM-YYYY HH24:MI:SS'), 'blabla', 0, -1);
-
+	"Id", "Category", "Priority", "Date", "Description", "TourId", "TouristId")
+	VALUES (-1, 'kate', 'prvi', '16-10-2024 10:10:10', 'blabla', 0, -1);
 INSERT INTO stakeholders."Problems"(
-    "Id", "Category", "Priority", "Date", "Description", "TourId", "TouristId")
-    VALUES (-2, 'gori', 'drugi', TO_TIMESTAMP('16-10-2024 10:10:10', 'DD-MM-YYYY HH24:MI:SS'), 'tratra', 1, -2);
-
+	"Id", "Category", "Priority", "Date", "Description", "TourId", "TouristId")
+	VALUES (-2, 'gori','drugi', '16-10-2024 10:10:10', 'tratra', 1, -2);
 INSERT INTO stakeholders."Problems"(
-    "Id", "Category", "Priority", "Date", "Description", "TourId", "TouristId")
-    VALUES (-3, 'ja', 'treci', TO_TIMESTAMP('16-10-2024 10:10:10', 'DD-MM-YYYY HH24:MI:SS'), 'rara', 2, -3);
-
+	"Id", "Category", "Priority", "Date", "Description", "TourId", "TouristId")
+	VALUES (-3, 'ja', 'treci', '16-10-2024 10:10:10', 'rara', 2, -3);
