2e95cbe43ebf35c00c8dcfd7b1dc409ddd9fdb70
Luka Vranjesevic <lukavranjesevic@gmail.com>
2024-11-02 13:34:40 +0100

    Location refactoring

--- src/Modules/Tours/Explorer.Tours.Core/Domain/Tours/Location.cs
+++ src/Modules/Tours/Explorer.Tours.Core/Domain/Tours/Location.cs
@@ -13,13 +13,15 @@ public class Location : ValueObject
     public double Latitude { get; }
     public double Longitude { get; }
     public string Country { get; }
+    public string City { get; }
 
     [JsonConstructor]
-    public Location(double latitude, double longitude, string country)
+    public Location(double latitude, double longitude, string country, string city)
     {
         Latitude = latitude;
         Longitude = longitude;
         Country = country;
+        City = city;
     }
 
     protected override IEnumerable<object> GetEqualityComponents()
