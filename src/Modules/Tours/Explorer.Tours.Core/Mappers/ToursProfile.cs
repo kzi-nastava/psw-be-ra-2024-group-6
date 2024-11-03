e2c9b8f7012c09a189f029d0495ad887756eb997
Luka Ivanisevic <lukaivanisevic10@gmail.com>
2024-11-03 13:56:28 +0100

    Added mapper for value objects

--- src/Modules/Tours/Explorer.Tours.Core/Mappers/ToursProfile.cs
+++ src/Modules/Tours/Explorer.Tours.Core/Mappers/ToursProfile.cs
@@ -2,9 +2,13 @@
 using Explorer.Tours.API.Dtos;
 using Explorer.Tours.API.Dtos.TourDtos;
 using Explorer.Tours.API.Dtos.TourDtos.CheckpointsDtos;
+using Explorer.Tours.API.Dtos.TourDtos.DistanceDtos;
+using Explorer.Tours.API.Dtos.TourDtos.DurationDtos;
 using Explorer.Tours.API.Dtos.TourDtos.LocationDtos;
 using Explorer.Tours.API.Dtos.TourDtos.ObjectDtos;
+using Explorer.Tours.API.Dtos.TourDtos.PriceDtos;
 using Explorer.Tours.Core.Domain;
+using Explorer.Tours.Core.Domain.ShoppingCarts;
 using Explorer.Tours.Core.Domain.Tours;
 using Object = Explorer.Tours.Core.Domain.Tours.Object;
 
@@ -21,5 +25,8 @@ public class ToursProfile : Profile
         CreateMap<TouristEquipmentManagerDto, TouristEquipmentManager>().ReverseMap();
 
         CreateMap<Tour, TourReadDto>().ReverseMap();
+        CreateMap<Price,PriceDto>().ReverseMap();
+        CreateMap<TourDuration, TourDurationDto>().ReverseMap();
+        CreateMap<Distance,DistanceDto>().ReverseMap();
     }
 }
