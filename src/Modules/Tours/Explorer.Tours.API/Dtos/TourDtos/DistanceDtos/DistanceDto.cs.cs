2cce94be35e495bc837212aceaf9b6d79fcb2510
Luka Ivanisevic <lukaivanisevic10@gmail.com>
2024-11-03 13:39:31 +0100

    Added value object dtos

---
+++ src/Modules/Tours/Explorer.Tours.API/Dtos/TourDtos/DistanceDtos/DistanceDto.cs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Tours.API.Dtos.TourDtos.DistanceDtos
+{
+    public class DistanceDto
+    {
+        public double Length { get; set; }
+        public string Unit { get; set; }
+    }
+}
--- src/Modules/Tours/Explorer.Tours.API/Dtos/TourDtos/TourDto.cs
+++ src/Modules/Tours/Explorer.Tours.API/Dtos/TourDtos/TourDto.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Explorer.Tours.API.Dtos.TourDtos.DistanceDtos;
+using Explorer.Tours.API.Dtos.TourDtos.DurationDtos;
+using Explorer.Tours.API.Dtos.TourDtos.PriceDtos;
 
 namespace Explorer.Tours.API.Dtos.TourDtos
 {
@@ -12,9 +15,12 @@ namespace Explorer.Tours.API.Dtos.TourDtos
         public string Name { get; set; }
         public string Description { get; set; }
         public string Difficulty { get; set; }
-        public List<string> Tags { get; set; } = new List<string>();
-        public double Cost { get; set; } = 0;
-        public string Status { get; set; } = "Draft";
+        public List<string> Tags { get; set; }
+        public PriceDto Price { get; set; }
+        public string Status { get; set; }
         public long? AuthorId { get; set; }
+        public DistanceDto TotalLength { get; set; }
+        public DateTime? StatusChangeTime { get; set; }
+        public List<TourDurationDto> Durations { get; set; }
     }
 }
