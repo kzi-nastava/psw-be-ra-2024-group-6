c8e0fa728f36e2bbfb4a4c9ec36467f9077674dc
Aleksandar Tomic <tomic.ra198.2021@uns.ac.rs>
2024-11-05 00:35:02 +0100

    Added funcionalities for the first week

---
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Core/Domain/Persons/Person.cs
@@ -0,0 +1,85 @@
+﻿using Explorer.BuildingBlocks.Core.Domain;
+using System.Linq;
+using System.Net.Mail;
+
+namespace Explorer.Stakeholders.Core.Domain.ProfileNotifications;
+
+public class Person : Entity
+{
+    public long UserId { get; private set; }
+    public string Name { get; private set; }
+    public string Surname { get; private set; }
+    public string Email { get; private set; }
+    public string Description { get; private set; }
+    public string Motto { get; private set; }
+    public string PictureURL { get; private set; }
+
+    public List<Follower> Followers { get; private set; } //ko tebe prati kao korisnika
+
+    public List<Follower> Followings { get; private set; } //
+
+
+    public Person() {
+        
+    }
+    public Person(long userId, string name, string surname, string email)
+    {
+        UserId = userId;
+        Name = name;
+        Surname = surname;
+        Email = email;
+        Description = "";
+        Motto = "";
+        PictureURL = "";
+        Followers = new List<Follower>();
+        Followings = new List<Follower>();
+        Validate();
+
+    }
+
+    public Person(long userId, string name, string surname, string email, string description, string motto, string picture)
+    {
+        UserId = userId;
+        Name = name;
+        Surname = surname;
+        Email = email;
+        Description = description;
+        Motto = motto;
+        PictureURL = picture;
+        Followers = new List<Follower>();
+        Followings = new List<Follower>();
+        Validate();
+
+    }
+    private void Validate()
+    {
+        if (UserId == 0) throw new ArgumentException("Invalid UserId");
+        if (string.IsNullOrWhiteSpace(Name)) throw new ArgumentException("Invalid Name");
+        if (string.IsNullOrWhiteSpace(Surname)) throw new ArgumentException("Invalid Surname");
+        if (!MailAddress.TryCreate(Email, out _)) throw new ArgumentException("Invalid Email");
+
+    }
+
+    public void AddFollower(int followerId)
+    {
+        if (Followers == null)
+        {
+            Followers = new List<Follower>();
+        }
+        
+            Followers.Add(new Follower(followerId));
+        
+
+    }
+
+    public void AddFollowing(int followingId)
+    {
+        if (Followings == null)
+        {
+            Followings = new List<Follower>();
+        }
+        
+            Followings.Add(new Follower(followingId));
+        
+    }
+}
---
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Core/Domain/RepositoryInterfaces/IFollowerRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Explorer.Stakeholders.Core.Domain.ProfileNotifications;
+
+namespace Explorer.Stakeholders.Core.Domain.RepositoryInterfaces
+{
+    public interface IFollowerRepository
+    {
+        List<Follower> GetFollowersByUserId(long userId);
+        void Add(Follower follower);
+        void Remove(Follower follower);
+    }
+}
