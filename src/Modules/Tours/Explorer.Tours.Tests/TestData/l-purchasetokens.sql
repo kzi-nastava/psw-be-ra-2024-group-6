3d8f2035690c08d6c4dfb91c757da17fa04e6f96
PINNGU <dzonic25@gmail.com>
2024-11-12 00:25:21 +0100

    Fixed 2 more tests

--- src/Modules/Tours/Explorer.Tours.Tests/TestData/l-purchasetokens.sql
+++ src/Modules/Tours/Explorer.Tours.Tests/TestData/l-purchasetokens.sql
@@ -1,7 +1,6 @@
 ﻿INSERT INTO tours."PurchaseTokens"(
-	"Id", "UserId", "TourId", "PurchaseDate", "isExpired")
-	VALUES (-1, -1,  -1, TO_TIMESTAMP('16-10-2024 10:10:10', 'DD-MM-YYYY HH24:MI:SS'),false);
-
-INSERT INTO tours."PurchaseTokens"(
-	"Id", "UserId", "TourId", "PurchaseDate", "isExpired")
-	VALUES (-2, -23,  -1, TO_TIMESTAMP('16-10-2024 10:10:10', 'DD-MM-YYYY HH24:MI:SS'),false);
+    "Id", "UserId", "TourId", "PurchaseDate","isExpired")
+    VALUES (-2, -23,  -1, TO_TIMESTAMP('16-10-2024 10:10:10', 'DD-MM-YYYY HH24:MI:SS'),false);
+    INSERT INTO tours."PurchaseTokens"(
+    "Id", "UserId", "TourId", "PurchaseDate","isExpired")
+    VALUES (-3, -21,  -1, TO_TIMESTAMP('16-10-2024 10:10:10', 'DD-MM-YYYY HH24:MI:SS'),false);
