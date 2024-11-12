8c8cde6ae3c0e9ba2e741fdf185174d17f7f3a22
Luka Ivanisevic <lukaivanisevic10@gmail.com>
2024-11-12 19:37:33 +0100

    Deleted unused tables

--- src/Modules/Stakeholders/Explorer.Stakeholders.Infrastructure/Database/StakeholdersContext.cs
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Infrastructure/Database/StakeholdersContext.cs
@@ -10,12 +10,8 @@ public class StakeholdersContext : DbContext
     public DbSet<User> Users { get; set; }
     public DbSet<Person> People { get; set; }
     public DbSet<Rating> Ratings { get; set; }
-
-    public DbSet<Tourist> Tourists { get; set; }
-	public DbSet<Problem> Problems { get; set; }
+    public DbSet<Problem> Problems { get; set; }
     public DbSet<Club> Clubs { get; set; }
-
-    public DbSet<Author> Author { get; set; }
 
     public DbSet<Notification> Notifications { get; set; }
 
@@ -53,16 +49,6 @@ public class StakeholdersContext : DbContext
             .WithOne()
             .HasForeignKey<Person>(s => s.UserId);
 
-
-        modelBuilder.Entity<Author>()
-            .HasOne<User>()
-            .WithOne()
-            .HasForeignKey<Author>(a => a.UserId);
-
-        /*modelBuilder.Entity<Tourist>()
-            .HasOne<User>()
-            .WithOne()
-            .HasForeignKey<Tourist>(t => t.UserId );*/
 		modelBuilder.Entity<Problem>()
             .HasOne<User>()
             .WithMany()
--- src/Modules/Stakeholders/Explorer.Stakeholders.Tests/TestData/a-delete.sql
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Tests/TestData/a-delete.sql
@@ -1,7 +1,6 @@
 DELETE FROM stakeholders."Users";
 DELETE FROM stakeholders."Ratings";
 DELETE FROM stakeholders."People";
-DELETE FROM stakeholders."Tourists";
 DELETE FROM stakeholders."Problems";
 DELETE FROM stakeholders."Clubs";
 DELETE FROM stakeholders."Notifications";
