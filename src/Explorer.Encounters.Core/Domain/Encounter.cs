dab2f7ec12e7a42d655585d41077fce68907a3a9
Aleksandar Tomic <tomic.ra198.2021@uns.ac.rs>
2024-11-19 16:12:24 +0100

    Added Model for EncounterModule

---
+++ src/Explorer.Encounters.Core/Domain/Encounter.cs
@@ -0,0 +1,40 @@
+﻿using Explorer.BuildingBlocks.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Encounters.Core.Domain
+{
+
+    public enum Status
+    {
+        Active,
+        Draft,
+        Archieved
+    }
+
+    public enum TypeEncounter { 
+        Social,
+        Location,
+        Misc
+    }
+
+    public class Encounter : Entity
+    {
+        public string Name {  get; private set; }
+        public string Description { get; private set; }
+
+        public Location Location { get; private set; }
+
+        public int Xp {  get; private set; }
+
+        public Status Status { get; private set; }
+
+        public TypeEncounter TypeEncounter { get; private set;
+        
+        }
+
+    }
+}
