7a65b42e5901389b15a807faf435ef6a150d7f84
MilanUD <149141569+MilanUD@users.noreply.github.com>
2024-11-28 19:30:03 +0100

    feat: added get for tours regardless of role and fixed a bug with comments

---
+++ src/Explorer.API/Controllers/Stakeholders/UserProfileController.cs
@@ -0,0 +1,33 @@
+﻿using Explorer.Stakeholders.Core.Domain;
+using Explorer.Tours.API.Dtos.TourDtos;
+using Explorer.Tours.API.Public;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Explorer.BuildingBlocks.Core.UseCases;
+using Explorer.Tours.API.Dtos;
+using FluentResults;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Explorer.API.Controllers.Stakeholders
+{
+    [Route("api/userProfile")]
+    public class UserProfileController : BaseApiController
+    {
+        private readonly ITourService _tourService;
+
+        public UserProfileController(ITourService tourService)
+        {
+            _tourService = tourService;
+        }
+
+        [HttpGet("{userId:long}")]
+        public ActionResult<List<TourDto>> GetByUserId(long userId)
+        {
+            var result = _tourService.GetByUserId(userId);
+
+            return CreateResponse(result);
+
+        }
+    }
+}
