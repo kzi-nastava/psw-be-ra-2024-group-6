42134cc1e1c7b3716e64466c17b772c6ea9aacd4
Luka Ivanisevic <lukaivanisevic10@gmail.com>
2024-12-20 01:27:00 +0100

    Added dtos

---
+++ src/Modules/Tours/Explorer.Tours.API/Dtos/TourDtos/RoadTripDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Explorer.Tours.API.Dtos.TourDtos.LocationDtos;
+
+namespace Explorer.Tours.API.Dtos.TourDtos
+{
+    public class RoadTripDto
+    {
+        public List<LatLngDto> RoadCoords { get; set; }
+        public LatLngDto NorthEastCoord { get; set; }
+        public LatLngDto SouthWestCoord { get; set; }
+    }
+}
