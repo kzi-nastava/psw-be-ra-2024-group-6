03bbd85e95ad49c463dbaa1f550677dc1b8fa862
Luka Ivanisevic <lukaivanisevic10@gmail.com>
2024-12-20 01:28:09 +0100

    Added search service and utils service for refactoring tour service

---
+++ src/Modules/Tours/Explorer.Tours.API/Public/ITourSearchService.cs
@@ -0,0 +1,22 @@
+﻿using Explorer.Tours.API.Dtos.TourDtos;
+using Explorer.Tours.API.Dtos;
+using FluentResults;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Tours.API.Public
+{
+    public interface ITourSearchService
+    {
+        Result<List<TourMapPreviewDto>> GetTourPreviewsOnMap(double latitude, double longitude);
+        Result<List<TourCardDto>> GetSearchedToursNearby(double latitude, double longitude, double radius);
+        Result<List<TourHoverMapDto>> FindToursOnMapNearby(double latitude, double longitude, double radius);
+        Result<List<AuthorLeaderboardDto>> GetAuthorLeaderboard(double latitude, double longitude, double radius);
+        Result<List<TourCardDto>> GetSortedTours(double latitude, double longitude, double radius, string criteria);
+        Result<List<TourCardDto>> GetFilteredTours(double latitude, double longitude, double radius, TourFilterDto tourFiltersDto);
+        Result<RoadTripHoverMapDto> GetRoadTripSuggestions(RoadTripDto roadTripDto, double roadRadius=1);
+    }
+}
---
+++ src/Modules/Tours/Explorer.Tours.Core/UseCases/Utils/Utils.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Tours.Core.UseCases.Utils
+{
+    public static class Utils
+    {
+        public static double CalculateHaversineDistance( double latitude, double longitude,double latitude2, double longitude2)
+        {
+            const double R = 6371; // Poluprečnik Zemlje u kilometrima
+            double dLat = DegreesToRadians(latitude2 - latitude);
+            double dLon = DegreesToRadians(longitude2 - longitude);
+
+            // Haversine formula
+            double a = Math.Sin(dLat / 2) * Math.Sin(dLat / 2) +
+                       Math.Cos(DegreesToRadians(latitude)) * Math.Cos(DegreesToRadians(latitude2)) *
+                       Math.Sin(dLon / 2) * Math.Sin(dLon / 2);
+            double c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+
+            return R * c; // Rezultat u kilometrima
+        }
+
+        public static double DegreesToRadians(double degrees)
+        {
+            return degrees * Math.PI / 180;
+        }
+    }
+}
