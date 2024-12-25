7a16c4507f0fb85aee8e356099cd3bdae1e74527
MilanUD <149141569+MilanUD@users.noreply.github.com>
2024-12-25 19:10:47 +0100

    fix: removed name property from blogPicture entity

--- src/Modules/Blog/Explorer.Blog.API/Dtos/BlogPictureDto.cs
+++ src/Modules/Blog/Explorer.Blog.API/Dtos/BlogPictureDto.cs
@@ -9,16 +9,14 @@ namespace Explorer.Blog.API.Dtos
     public class BlogPictureDto
     {
         public int Id {  get; set; }
-        public string Name { get; set; }
         public string Data { get; set; }
 
         public int BlogId { get; set; }
 
         
         public BlogPictureDto() { }
-        public BlogPictureDto(string name, int blogId, string data)
+        public BlogPictureDto(int blogId, string data)
         {
-            Name = name;
             BlogId = blogId;
             Data = data;
         }
--- src/Modules/Blog/Explorer.Blog.Core/Domain/Blogs/BlogPicture.cs
+++ src/Modules/Blog/Explorer.Blog.Core/Domain/Blogs/BlogPicture.cs
@@ -9,15 +9,13 @@ namespace Explorer.Blog.Core.Domain.Blogs
 {
     public class BlogPicture : Entity
     {
-        public string Name { get; private set; }
         public string Data { get; private set; }
         public long BlogId { get; private set; }
         public Blog Blog { get; private set; }
 
         public BlogPicture() { }
-        public BlogPicture(string name, long blogId, string data, Blog blog = null)
+        public BlogPicture(long blogId, string data, Blog blog = null)
         {
-            Name = name;
             BlogId = blogId;
             Blog = blog;
             Data = data;
